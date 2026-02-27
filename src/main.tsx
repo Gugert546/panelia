ccf74d4bd5ef3620735a26da02e80202ab51a2f8
Siddi-99 <sindres99@gmail.com>
2026-02-27 11:50:00 +0100

    Changes er gjort

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-
-// Denne filen sin eneste hensikt er å starte appen / nettsiden. Ingenting annet. 
-
 import { createRoot } from 'react-dom/client'
 import "react-calendar/dist/Calendar.css";
 import './index.css'
-import App from '../src/app/App'
-
+import App from './app/App'
+import { AuthProvider } from './app/providers/authProvider'
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 )
-
-import "./test-news"; // <-- Midlertidig linje for å teste News-api call
