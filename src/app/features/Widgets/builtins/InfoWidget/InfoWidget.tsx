45d1abbebbb42ba554333b77c7875a5efe80f783
gugert546 <halstensen55@gmail.com>
2026-04-13 10:11:01 +0200

    infowidget følger nå språkoppsett, lett stylet

--- src/app/features/Widgets/builtins/InfoWidget/InfoWidget.tsx
+++ src/app/features/Widgets/builtins/InfoWidget/InfoWidget.tsx
@@ -1,7 +1,12 @@
 import WidgetContainer from "../../components/WidgetContainer";
 import WidgetPane from "../../components/WidgetPane";
+import { useLanguage } from "../../../../providers/languageProvider";
+import { useFontSize } from "../../../../providers/themeProviders";
 
 export default function InfoWidget() {
+  const { t } = useLanguage();
+  const { fontSize } = useFontSize();
+  const titleSize = fontSize+2;
   return (
     <WidgetContainer>
       <WidgetPane title="">
@@ -10,16 +15,28 @@ export default function InfoWidget() {
             width: "100%",
             height: "100%",
             display: "flex",
-            alignItems: "center",
+            alignItems: "top",
             justifyContent: "center",
             textAlign: "center",
             fontWeight: 700,
-            fontSize: "clamp(1rem, 2.2vw, 1.5rem)",
+            fontSize: titleSize
           }}
         >
-          Velkommen til Panelia!
-          
-          Bruk siden vår hvis ikke får du besøk i natt! 
+          <div>
+            <h1>{t("widgets.infoWidget.title")}</h1>
+              <div
+              style={{
+                padding: "8px 10px",
+                marginTop:8,
+                borderRadius: 8,
+                border: "1px solid #ddd",
+                background: "#fff",
+                fontSize,
+              }}
+              >
+                {t("widgets.infoWidget.bread")}
+              </div>
+          </div>
         </div>
       </WidgetPane>
     </WidgetContainer>
