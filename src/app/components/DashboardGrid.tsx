3e1411eecd5e491402a45265edf9e23257543783
gugert546 <halstensen55@gmail.com>
2026-04-27 15:21:01 +0200

    custom knapper kan nå dras fra hele knappen

--- src/app/components/DashboardGrid.tsx
+++ src/app/components/DashboardGrid.tsx
@@ -175,7 +175,7 @@ export default function DashboardGrid({
       width={window.innerWidth - sidebarWidth}
       isDraggable={isMovable}
       isResizable={isMovable}
-      draggableCancel="input,button,select,option,textarea,label,[role='button'],[contenteditable='true'],.widget-lock-btn,.widget-clock-mode-btn,.widget-style-btn,.widget-style-control"
+      draggableCancel="input,button:not(.widget-draggable-button),select,option,textarea,label,[role='button']:not(.widget-draggable-button),[contenteditable='true'],.widget-lock-btn,.widget-clock-mode-btn,.widget-style-btn,.widget-style-control"
       compactType={null}
       preventCollision={true}  // blokkerer auto-flytting av andre widgets ved hover / drag
       margin={[0, 0]}    
